fc7bca6d0f3f19410448ad86a3567f44a82b52b5
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-05-09 23:19:26 +0200

    Add some more support to queries

--- src/main/java/org/example/kompilatory/MyVisitor.java
+++ src/main/java/org/example/kompilatory/MyVisitor.java
@@ -10,6 +10,7 @@ public class MyVisitor extends SQLParserBaseVisitor<String> {
 
     @Override
     public String visitSelect_core(SQLParser.Select_coreContext ctx) {
+        String tableAlias = "row"; //TODO
         String wherePart = "";
         if(ctx.whereExpr != null && ctx.WHERE_() != null) {
             wherePart = "WHERE " + visitWhereExpr(ctx.whereExpr) + " \n";
@@ -17,8 +18,9 @@ public class MyVisitor extends SQLParserBaseVisitor<String> {
 
         String groupByPart = "";
         if(ctx.GROUP_BY() != null && ctx.groupByExpr != null && !ctx.groupByExpr.isEmpty()) {
-            groupByPart = visitExprListAndConcatByComma(ctx.groupByExpr);
-            //TODO: Change into LINQ form
+            groupByPart = "GROUP " + tableAlias + " by new {";
+            groupByPart += visitExprListAndConcatByComma(ctx.groupByExpr);
+            groupByPart += "} into g \n";
         }
 
         String fromOrJoinPart = "";
@@ -26,23 +28,30 @@ public class MyVisitor extends SQLParserBaseVisitor<String> {
             fromOrJoinPart = visitExprListAndConcatByComma(ctx.table_or_subquery());
         }
         else if(ctx.join_clause() != null) {
-            //TODO
+            fromOrJoinPart = visit(ctx.join_clause());
         }
         else
             throw new RuntimeException("Brakuje czegoś we FROM");
 
-        return "FROM " + fromOrJoinPart + " \n"
+        return "FROM " + tableAlias + " in " + fromOrJoinPart + " \n"
                 + wherePart
+                + groupByPart
                 + "SELECT " + getResultColumnsString(ctx)
                 + ";";
 
+    }
+
+    @Override
+    public String visitJoin_clause(SQLParser.Join_clauseContext ctx) {
+        // TODO: Tutaj zmieniaj Krzysztof
+        return super.visitJoin_clause(ctx);
     }
 
     String visitWhereExpr(SQLParser.ExprContext ctx) {
         StringBuilder builder = new StringBuilder();
 
         for(int i = 0; i < ctx.getChildCount(); ++i) {
-            builder.append(ctx.getChild(i).getText()).append(" ");
+            builder.append(visit(ctx.getChild(i))).append(" ");
         }
 
         return builder.toString();
@@ -54,7 +63,7 @@ public class MyVisitor extends SQLParserBaseVisitor<String> {
 
     private <T extends ParserRuleContext> String visitExprListAndConcatByComma(List<T> ctxList) {
         List<String> colNames = ctxList.stream().map(this::visit).collect(Collectors.toList());
-        return colNames.stream().reduce((acc, next) -> acc + "," + next).get();
+        return colNames.stream().reduce((acc, next) -> acc + ", " + next).get();
     }
 
     // Trzeba dać aby łączyło w stringi nowe rezultaty
@@ -74,6 +83,8 @@ public class MyVisitor extends SQLParserBaseVisitor<String> {
     public String visitTerminal(TerminalNode node) {
         if(node.getText().equals("<EOF>")) //Żeby nie wypisywało <EOF> na końcu
             return "";
+        if(node.getText().equals("AND"))
+            return "&&";
 
         return node.getText(); // zwraca tekst tokenu
     }
